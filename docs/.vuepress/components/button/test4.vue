9b3d9c00068413f0cd158836276a9e358df50fc9
zhangyanling77 <18783226594@163.com>
2020-03-31 13:04:14 +0800

    feat: demo4

---
+++ docs/.vuepress/components/button/test4.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <zyl-button-group>
+      <zyl-button type="primary" icon="left" icon-position="left">上一页</zyl-button>
+      <zyl-button type="primary" icon="right" icon-position="right">下一页</zyl-button>
+    </zyl-button-group>
+    <zyl-button-group>
+      <zyl-button icon="search"></zyl-button>
+      <zyl-button icon="setting"></zyl-button>
+      <zyl-button icon="edit"></zyl-button>
+    </zyl-button-group>
+  </div>
+</template>
