f9e6867634d9145fa48d0c119f61c3adaca94b08
zhangyanling77 <18783226594@163.com>
2020-03-31 13:02:15 +0800

    feat：demo2

---
+++ docs/.vuepress/components/button/test2.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+      <zyl-button icon="edit" type="primary">主要按钮</zyl-button>
+      <zyl-button icon="edit" type="success" icon-position="right">危险按钮</zyl-button>
+      <zyl-button icon="search" type="danger"></zyl-button>
+  </div>
+</template>
