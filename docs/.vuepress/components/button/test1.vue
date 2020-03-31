7f66822b7d35503e9361038f5c6c8590acfe0dc3
zhangyanling77 <18783226594@163.com>
2020-03-31 13:01:41 +0800

    feat：demo1

---
+++ docs/.vuepress/components/button/test1.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+      <zyl-button>默认按钮</zyl-button>
+      <zyl-button type="primary">主要按钮</zyl-button>
+      <zyl-button type="success">成功按钮</zyl-button>
+      <zyl-button type="info">信息按钮</zyl-button>
+      <zyl-button type="warning">警告按钮</zyl-button>
+      <zyl-button type="danger">危险按钮</zyl-button> 
+  </div>
+</template>
