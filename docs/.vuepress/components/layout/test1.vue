5e73be8f336d0701bc8e07a878a9c77a2296f226
zhangyanling77 <18783226594@163.com>
2020-04-07 18:24:47 +0800

    Update test1.vue

--- docs/.vuepress/components/layout/test1.vue
+++ docs/.vuepress/components/layout/test1.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    
+    <zyl-row>
+      <zyl-col :span="24">
+        <div class="grid-content bg-purple-dark">col-24</div>
+      </zyl-col>
+    </zyl-row>
   </div>
 </template>
+
+<style lang="scss">
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+</style>
