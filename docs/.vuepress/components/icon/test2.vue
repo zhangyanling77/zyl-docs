099b19f8d0774ccf97aa7b73bfe0bbbc4496ba8f
zhangyanling77 <18783226594@163.com>
2020-04-07 17:21:54 +0800

    docs: icon demo all

--- docs/.vuepress/components/icon/test2.vue
+++ docs/.vuepress/components/icon/test2.vue
@@ -1,62 +1,246 @@
 <template>
-  <div>
-    <zyl-icon icon="check-circle"></zyl-icon>
-    <zyl-icon icon="close-circle"></zyl-icon>
-    <zyl-icon icon="info-circle"></zyl-icon>
-    <zyl-icon icon="left-circle"></zyl-icon>
-    <zyl-icon icon="down-circle"></zyl-icon>
-    <zyl-icon icon="minus-circle"></zyl-icon>
-    <zyl-icon icon="plus-circle"></zyl-icon>
-    <zyl-icon icon="question-circle"></zyl-icon>
-    <zyl-icon icon="right-circle"></zyl-icon>
-    <zyl-icon icon="up-circle"></zyl-icon>
-    <zyl-icon icon="up-circle"></zyl-icon>
-    <zyl-icon icon="warning-circle"></zyl-icon>
-    <zyl-icon icon="sync"></zyl-icon>
-    <zyl-icon icon="setting"></zyl-icon>
-    <zyl-icon icon="location"></zyl-icon>
-    <zyl-icon icon="edit-square"></zyl-icon>
-    <zyl-icon icon="check-square"></zyl-icon>
-    <zyl-icon icon="user"></zyl-icon>
-    <zyl-icon icon="image"></zyl-icon>
-    <zyl-icon icon="heart"></zyl-icon>
-    <zyl-icon icon="edit"></zyl-icon>
-    <zyl-icon icon="key"></zyl-icon>
-    <zyl-icon icon="verticalright"></zyl-icon>
-    <zyl-icon icon="verticalleft"></zyl-icon>
-    <zyl-icon icon="search"></zyl-icon>
-    <zyl-icon icon="right"></zyl-icon>
-    <zyl-icon icon="left"></zyl-icon>
-    <zyl-icon icon="up"></zyl-icon>
-    <zyl-icon icon="down"></zyl-icon>
-    <zyl-icon icon="fullscreen"></zyl-icon>
-    <zyl-icon icon="fullscreen-exit"></zyl-icon>
-    <zyl-icon icon="arrowright"></zyl-icon>
-    <zyl-icon icon="arrowup"></zyl-icon>
-    <zyl-icon icon="arrowleft"></zyl-icon>
-    <zyl-icon icon="arrowdown"></zyl-icon>
-    <zyl-icon icon="upload"></zyl-icon>
-    <zyl-icon icon="download"></zyl-icon>
-    <zyl-icon icon="check"></zyl-icon>
-    <zyl-icon icon="close"></zyl-icon>
-    <zyl-icon icon="check-circle-fill"></zyl-icon>
-    <zyl-icon icon="left-circle-fill"></zyl-icon>
-    <zyl-icon icon="down-circle-fill"></zyl-icon>
-    <zyl-icon icon="minus-circle-fill"></zyl-icon>
-    <zyl-icon icon="close-circle-fill"></zyl-icon>
-    <zyl-icon icon="info-circle-fill"></zyl-icon>
-    <zyl-icon icon="up-circle-fill"></zyl-icon>
-    <zyl-icon icon="right-circle-fill"></zyl-icon>
-    <zyl-icon icon="plus-circle-fill"></zyl-icon>
-    <zyl-icon icon="question-circle-fill"></zyl-icon>
-    <zyl-icon icon="warning-circle-fill"></zyl-icon>
-    <zyl-icon icon="check-square-fill"></zyl-icon>
-    <zyl-icon icon="down-square-fill"></zyl-icon>
-    <zyl-icon icon="minus-square-fill"></zyl-icon>
-    <zyl-icon icon="close-square-fill"></zyl-icon>
-    <zyl-icon icon="left-square-fill"></zyl-icon>
-    <zyl-icon icon="up-square-fill"></zyl-icon>
-    <zyl-icon icon="right-square-fill"></zyl-icon>
-    <zyl-icon icon="plus-square-fill"></zyl-icon>
+  <div class="wrapper">
+    <div class="card">
+      <zyl-icon icon="check-circle"></zyl-icon>
+      <span>icon-check-circle</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="close-circle"></zyl-icon>
+      <span>icon-close-circle</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="info-circle"></zyl-icon>
+      <span>icon-info-circle</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="left-circle"></zyl-icon>
+      <span>icon-left-circle</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="down-circle"></zyl-icon>
+      <span>icon-down-circle</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="minus-circle"></zyl-icon>
+      <span>icon-minus-circle</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="plus-circle"></zyl-icon>
+      <span>icon-plus-circle</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="question-circle"></zyl-icon>
+      <span>icon-question-circle</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="right-circle"></zyl-icon>
+      <span>icon-right-circle</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="up-circle"></zyl-icon>
+      <span>icon-up-circle</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="warning-circle"></zyl-icon>
+      <span>icon-warning-circle</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="sync"></zyl-icon>
+      <span>icon-sync</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="setting"></zyl-icon>
+      <span>icon-setting</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="location"></zyl-icon>
+      <span>icon-location</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="edit-square"></zyl-icon>
+      <span>icon-edit-square</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="check-square"></zyl-icon>
+      <span>icon-check-square</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="user"></zyl-icon>
+      <span>icon-user</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="image"></zyl-icon>
+      <span>icon-image</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="heart"></zyl-icon>
+      <span>icon-heart</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="edit"></zyl-icon>
+      <span>icon-edit</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="key"></zyl-icon>
+      <span>icon-key</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="verticalright"></zyl-icon>
+      <span>icon-verticalright</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="verticalleft"></zyl-icon>
+      <span>icon-verticalleft</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="search"></zyl-icon>
+      <span>icon-search</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="right"></zyl-icon>
+      <span>icon-right</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="left"></zyl-icon>
+      <span>icon-left</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="up"></zyl-icon>
+      <span>icon-up</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="down"></zyl-icon>
+      <span>icon-down</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="fullscreen"></zyl-icon>
+      <span>icon-fullscreen</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="fullscreen-exit"></zyl-icon>
+      <span>icon-fullscreen-exit</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="arrowright"></zyl-icon>
+      <span>icon-arrowright</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="arrowup"></zyl-icon>
+      <span>icon-arrowup</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="arrowleft"></zyl-icon>
+      <span>icon-arrowleft</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="arrowdown"></zyl-icon>
+      <span>icon-arrowdown</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="upload"></zyl-icon>
+      <span>icon-upload</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="download"></zyl-icon>
+      <span>icon-download</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="check"></zyl-icon>
+      <span>icon-check</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="close"></zyl-icon>
+      <span>icon-close</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="check-circle-fill"></zyl-icon>
+      <span>icon-check-circle-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="left-circle-fill"></zyl-icon>
+      <span>icon-left-circle-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="down-circle-fill"></zyl-icon>
+      <span>icon-down-circle-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="minus-circle-fill"></zyl-icon>
+      <span>icon-minus-circle-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="close-circle-fill"></zyl-icon>
+      <span>icon-close-circle-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="info-circle-fill"></zyl-icon>
+      <span>icon-info-circle-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="up-circle-fill"></zyl-icon>
+      <span>icon-up-circle-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="right-circle-fill"></zyl-icon>
+      <span>icon-right-circle-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="plus-circle-fill"></zyl-icon>
+      <span>icon-plus-circle-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="question-circle-fill"></zyl-icon>
+      <span>icon-question-circle-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="warning-circle-fill"></zyl-icon>
+      <span>icon-warning-circle-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="check-square-fill"></zyl-icon>
+      <span>icon-check-square-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="down-square-fill"></zyl-icon>
+      <span>icon-down-square-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="minus-square-fill"></zyl-icon>
+      <span>icon-minus-square-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="close-square-fill"></zyl-icon>
+      <span>icon-close-square-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="left-square-fill"></zyl-icon>
+      <span>icon-left-square-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="up-square-fill"></zyl-icon>
+      <span>icon-up-square-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="right-square-fill"></zyl-icon>
+      <span>icon-right-square-fill</span>
+    </div>
+    <div class="card">
+      <zyl-icon icon="plus-square-fill"></zyl-icon>
+      <span>icon-plus-square-fill</span>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.wrapper {
+  .card {
+    display: inline-flex;
+    border: 1px solid #d9d9d9;
+    border-radius: 4px;
+    flex-direction: column;
+    flex: 1;
+    padding: 16px;
+    margin: 0 10px 14px 0;
+  }
+}
+</style>
