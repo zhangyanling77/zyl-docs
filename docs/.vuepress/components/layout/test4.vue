7ed9f4cdb2afeb1c19b78d7374ef96b049598c73
zhangyanling77 <18783226594@163.com>
2020-04-08 13:57:11 +0800

    docs: layout 对齐方式

---
+++ docs/.vuepress/components/layout/test4.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <zyl-row justify="center">
+      <zyl-col :span="4">
+        <div class="grid-content bg-purple-dark"></div>
+      </zyl-col>
+      <zyl-col :span="4">
+        <div class="grid-content bg-purple"></div>
+      </zyl-col>
+    </zyl-row>
+    <br/>
+    <zyl-row justify="space-between">
+      <zyl-col :span="4">
+        <div class="grid-content bg-purple-dark"></div>
+      </zyl-col>
+      <zyl-col :span="4">
+        <div class="grid-content bg-purple"></div>
+      </zyl-col>
+    </zyl-row>
+  </div>
+</template>
+
+<style lang="scss">
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+</style>
