5c10d94730a7555a0954efe823d6ac23cfa9f82a
zhangyanling77 <18783226594@163.com>
2020-04-08 13:56:09 +0800

    docs: layout 分栏偏移

---
+++ docs/.vuepress/components/layout/test3.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <zyl-row>
+      <zyl-col :span="6">
+        <div class="grid-content bg-purple-dark"></div>
+      </zyl-col>
+      <zyl-col :span="6" :offset="12">
+        <div class="grid-content bg-purple"></div>
+      </zyl-col>
+    </zyl-row>
+  </div>
+</template>
+
+<style lang="scss">
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+</style>
