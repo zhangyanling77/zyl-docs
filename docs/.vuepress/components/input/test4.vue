e18643d7e614956d5e891f9b6328772dc404766b
zhangyanling77 <18783226594@163.com>
2020-05-06 13:53:19 +0800

    feat: input password

---
+++ docs/.vuepress/components/input/test4.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <zyl-input 
+      v-model="value"
+      type="password"
+      placeholder="请输入内容" 
+      name="password"
+    >
+    </zyl-input>
+    <zyl-input 
+      v-model="value"
+      type="password"
+      placeholder="请输入内容" 
+      name="password"
+      show-password
+    >
+    </zyl-input>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
