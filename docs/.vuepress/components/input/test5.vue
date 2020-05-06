5f8ee86d71aca348cdb2a9cffc903b215d6905e7
zhangyanling77 <18783226594@163.com>
2020-05-06 13:54:17 +0800

    feat: input prefix/suffix icon

---
+++ docs/.vuepress/components/input/test5.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <zyl-input 
+      v-model="value"
+      type="text"
+      placeholder="请输入内容" 
+      name="username"
+      prefix-icon="edit"
+    >
+    </zyl-input>
+    
+    <zyl-input 
+      v-model="value"
+      type="text"
+      placeholder="请输入内容" 
+      name="username"
+      suffix-icon="edit"
+    >
+    </zyl-input>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
