757a0e84a26225a76d1bbfba097af8c939bed144
zhangyanling77 <18783226594@163.com>
2020-05-06 13:52:07 +0800

    feat: input disabled

---
+++ docs/.vuepress/components/input/test2.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <zyl-input 
+      v-model="value"
+      type="text"
+      :disabled="true"
+      placeholder="请输入内容" 
+      name="username"
+    >
+    </zyl-input>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: 'zhangyanling'
+    }
+  }
+}
+</script>
