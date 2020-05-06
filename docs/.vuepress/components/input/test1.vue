a2a81045e3d76f78545687ee174e4fee4e11ec59
zhangyanling77 <18783226594@163.com>
2020-05-06 13:51:31 +0800

    feat: input basic

--- docs/.vuepress/components/input/test1.vue
+++ docs/.vuepress/components/input/test1.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    input
+    <zyl-input 
+      v-model="value"
+      type="text"
+      placeholder="请输入内容" 
+      name="username"
+    >
+    </zyl-input>
+    {{value}}
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
