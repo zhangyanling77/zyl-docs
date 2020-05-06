d11cbf30fee3bf2ae571f8c6a56a3d320a21630a
zhangyanling77 <18783226594@163.com>
2020-05-06 13:52:48 +0800

    feat: input clearable

---
+++ docs/.vuepress/components/input/test3.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <zyl-input 
+      v-model="value"
+      type="text"
+      placeholder="请输入内容" 
+      name="username"
+      clearable
+    >
+    </zyl-input>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
