117217008bf6838071c74f37042e0ecbc348e56e
zhangyanling77 <18783226594@163.com>
2020-05-08 10:26:01 +0800

    feat: pagination demo

--- docs/.vuepress/components/pagination/test1.vue
+++ docs/.vuepress/components/pagination/test1.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    pagination
+    <zyl-pagination
+      :total="10"
+      :pager-count="7"
+      :current-page.sync="currentPage"
+    ></zyl-pagination>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentPage: 5
+    }
+  }
+}
+</script>
