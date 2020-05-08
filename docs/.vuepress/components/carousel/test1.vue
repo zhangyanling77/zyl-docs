a7fdc7167b3d5fd59823a502869d172b5a839520
zhangyanling77 <18783226594@163.com>
2020-05-08 12:45:48 +0800

    feat: carousel basic

--- docs/.vuepress/components/carousel/test1.vue
+++ docs/.vuepress/components/carousel/test1.vue
@@ -1,5 +1,53 @@
 <template>
   <div>
-    carousel
+    <zyl-carousel
+      height="240px"
+      :autoplay="true"
+      :delay="2000"
+      :initial-index="0"
+      :loop="true"
+      @change="change"
+    >
+      <zyl-carousel-item>
+        <div class="content bg-purple-dark">
+          内容1
+        </div>
+      </zyl-carousel-item>
+      <zyl-carousel-item>
+        <div class="content bg-purple">
+          内容2
+        </div>
+      </zyl-carousel-item>
+      <zyl-carousel-item>
+        <div class="content bg-purple-dark">
+          内容3
+        </div>
+      </zyl-carousel-item>
+    </zyl-carousel>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    change(index) {
+      console.log(index)
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .content {
+    width: 100%;
+    height: 100%;
+    text-align: center;
+  }
+</style>
+
