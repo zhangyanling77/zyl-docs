553dd9187f30d2e8b3c30270b6ae7657138a21ef
zhangyanling77 <18783226594@163.com>
2020-05-08 10:11:21 +0800

    feat: progress demo

--- docs/.vuepress/components/progress/test1.vue
+++ docs/.vuepress/components/progress/test1.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    progress
+    <zyl-progress 
+      :strokeWidth="10"
+      :percentage="percent"
+      color="#409EFF"
+    ></zyl-progress>
+    <div>
+      当前进度：{{percent}}
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      percent: 50
+    }
+  }
+}
+</script>
