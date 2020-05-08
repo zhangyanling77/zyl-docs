d6f62719387816a0821f2119e4ef3ca1c752506d
zhangyanling77 <18783226594@163.com>
2020-05-08 13:10:11 +0800

    feat: infinite-scroll basic

--- docs/.vuepress/components/infinite-scroll/test1.vue
+++ docs/.vuepress/components/infinite-scroll/test1.vue
@@ -1,5 +1,43 @@
 <template>
-  <div>
-    infinite-scroll
+  <div style="overflow-y:scroll;border:1px solid #ddd;" >
+    <ul
+      v-infinite-scroll="load"
+      infinite-scroll-disabled="hasMore"
+      infinite-scroll-delay="delay"
+      infinite-scroll-distance="distance"
+      infinite-scroll-immediate="immediate"
+      class="box"
+    >
+      <li v-for="c in count" :key="c">{{c}}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      hasMore: false,
+      delay: 200,
+      distance: 30,
+      immediate: true,
+    }
+  },
+  methods: {
+    load() {
+      this.count += 2
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+ul {
+  list-style: none;
+}
+.box {
+  width: 400px;
+  height: 300px;
+}
+</style>
