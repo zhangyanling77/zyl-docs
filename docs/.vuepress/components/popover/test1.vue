10f0b578b14c220936cfe1d2433781932ecef950
zhangyanling77 <18783226594@163.com>
2020-05-08 11:21:33 +0800

    feat: popover basic

--- docs/.vuepress/components/popover/test1.vue
+++ docs/.vuepress/components/popover/test1.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
-    popover
+    <zyl-popover
+      v-model="popVal1"
+      width="200px"
+      title="标题"
+      content="内容，这是一个popover"
+      trigger="click"
+      placement="bottom"
+    >
+      <zyl-button slot="reference" type="primary">click激活</zyl-button>
+    </zyl-popover>
+
+    <zyl-popover
+      v-model="popVal2"
+      width="200px"
+      title="标题"
+      content="内容，这是一个popover"
+      trigger="hover"
+      placement="top"
+    >
+      <zyl-button slot="reference" type="primary">hover激活</zyl-button>
+    </zyl-popover>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      popVal1: false,
+      popVal2: false
+    }
+  }
+}
+</script>
