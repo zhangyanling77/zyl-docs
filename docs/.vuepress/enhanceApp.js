ef05a47f7f29c4ae3ca6956c5f644a8314716a1e
zhangyanling77 <18783226594@163.com>
2021-01-15 15:16:04 +0800

    Update enhanceApp.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,14 +1,12 @@
 import Vue from 'vue';
 import Element from 'element-ui'; // 引入elementUi
 import 'element-ui/lib/theme-chalk/index.css';
-
 import hljs from 'highlight.js';
 import 'highlight.js/styles/googlecode.css'; //样式文件
-
 import zylUI from 'zyl-ui'; // 要编写对应的文档的包
 import 'zyl-ui/dist/zyl-ui.css';
-
-Vue.directive('highlight',function (el) {
+// 注册指令
+Vue.directive('highlight', function (el) {
   let blocks = el.querySelectorAll('pre code');
   blocks.forEach((block)=>{
     hljs.highlightBlock(block);
@@ -25,4 +23,4 @@ export default ({
   // 做一些其他的应用级别的优化
   Vue.use(Element);
   Vue.use(zylUI);
-}
+};
