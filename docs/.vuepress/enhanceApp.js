358c126062fd070001dd5ca4e0fa84c196b1024d
zhangyanling77 <18783226594@163.com>
2021-01-15 15:14:37 +0800

    Update enhanceApp.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -11,16 +11,18 @@ import 'zyl-ui/dist/zyl-ui.css';
 Vue.directive('highlight',function (el) {
   let blocks = el.querySelectorAll('pre code');
   blocks.forEach((block)=>{
-    hljs.highlightBlock(block)
+    hljs.highlightBlock(block);
   })
-})
+});
 
 export default ({
-  Vue,
-  options, 
-  router,
-  siteData
+  Vue, // VuePress 正在使用的 Vue 构造函数
+  options, // 附加到根实例的一些选项
+  router, // 当前应用的路由实例
+  siteData, // 站点元数据
+  isServer // 当前应用配置是处于 服务端渲染 或 客户端
 }) => {
+  // 做一些其他的应用级别的优化
   Vue.use(Element);
   Vue.use(zylUI);
 }
